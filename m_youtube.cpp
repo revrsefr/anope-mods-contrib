ae152d64c4da25f992f85824db6c4c255adb6093
reverse <129334336+revrsefr@users.noreply.github.com>
2025-01-10 18:39:57 +0100

    Update m_youtube.cpp

--- m_youtube.cpp
+++ m_youtube.cpp
@@ -1,3 +1,20 @@
+/*
+2024 Jean "reverse" Chevronnet
+Module for Anope IRC Services v2.1.
+Botserv read and answer youtube links.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /// $CompilerFlags: find_compiler_flags("RapidJSON")
 /// $LinkerFlags: find_linker_flags("RapidJSON")
 /// $CompilerFlags: find_compiler_flags("libcurl")
