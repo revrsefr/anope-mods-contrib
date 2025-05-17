387510ffed62d1b85782e6ffbeb31877552d7b9b
reverse <129334336+revrsefr@users.noreply.github.com>
2025-05-17 09:22:42 +0200

    Update m_saslJwt.cpp
    Update to 2.1.15

--- m_saslJwt.cpp
+++ m_saslJwt.cpp
@@ -17,9 +17,7 @@
 #include <openssl/evp.h>  // Explicitly include OpenSSL headers
 
 // JWT mechanism that handles JWT tokens
-class JWT final
-	: public SASL::Mechanism
-{
+class JWT final : public SASL::Mechanism {
 public:
 	JWT(Module *o)
 		: SASL::Mechanism(o, "JWT")
@@ -51,8 +49,8 @@ public:
 				// Decode the JWT token.
 				auto decoded = jwt::decode(token_str);
 				// Set your shared secret and expected issuer.
-				std::string secret = "YOURE SECRET SOON WILL BE IN MODULES.CONF :p";
-				std::string expected_issuer = "ME YOU ARE THE USSUER";
+				std::string secret = "9LbwzoQMkbS4yKR2HUMpKZDuZZJ3TsDvp+gza9zYzFpvL+pLk1w07R7ZQ8ZrJGm5Sr9lbFiiPK14JCasK4gQxA==T";
+				std::string expected_issuer = "irc.t-chat.fr";
 				// Verify the token using HS256.
 				jwt::verify()
 					.allow_algorithm(jwt::algorithm::hs256{secret})
@@ -87,16 +85,13 @@ public:
 };
 
 // Enhanced PLAIN mechanism that handles both passwords and JWT tokens
-class JWTEnhancedPlain final
-	: public SASL::Mechanism
+class JWTEnhancedPlain final : public SASL::Mechanism
 {
 private:
 	bool looks_like_jwt(const Anope::string &token) const
 	{
-		// Very basic check - JWT tokens typically have two dots separating 
-		// header, payload, and signature
-		return token.find('.') != Anope::string::npos && 
-			   token.rfind('.') != token.find('.');
+		// Very basic check - JWT tokens typically have two dots separating header, payload, and signature.
+		return token.find('.') != Anope::string::npos && token.rfind('.') != token.find('.');
 	}
 	
 	bool verify_jwt(const Anope::string &token_str, std::string &subject)
@@ -106,8 +101,8 @@ private:
 			auto decoded = jwt::decode(token_str.c_str());
 			
 			// Verify the token
-			std::string secret = "next update this will be in modules.conf";
-			std::string expected_issuer = "issuer";
+			std::string secret = "9LbwzoQMkbS4yKR2HUMpKZDuZZJ3TsDvp+gza9zYzFpvL+pLk1w07R7ZQ8ZrJGm5Sr9lbFiiPK14JCasK4gZxA==T";
+			std::string expected_issuer = "irc.t-chat.fr";
 			
 			jwt::verify()
 				.allow_algorithm(jwt::algorithm::hs256{secret})
@@ -152,22 +147,22 @@ public:
 			Anope::string authzid = message.substr(0, zcsep);
 			Anope::string authcid = message.substr(zcsep + 1, cpsep - zcsep - 1);
 
-			// We don't support having an authcid that is different to the authzid.
+			// We don't support having an authcid that is different from authzid.
 			if (!authzid.empty() && authzid != authcid)
 				return false;
 
 			Anope::string passwd = message.substr(cpsep + 1);
-
-			if (authcid.empty() || passwd.empty() || !IRCD->IsNickValid(authcid) || passwd.find_first_of("\r\n\0") != Anope::string::npos)
+			if (authcid.empty() || passwd.empty() || !IRCD->IsNickValid(authcid) ||
+			    passwd.find_first_of("\r\n\0") != Anope::string::npos)
 				return false;
 
-			// Check if password looks like a JWT token
+			// Check if password looks like a JWT token.
 			if (looks_like_jwt(passwd))
 			{
 				std::string subject;
 				if (verify_jwt(passwd, subject))
 				{
-					// Find the NickAlias by the JWT subject
+					// Find the NickAlias by the JWT subject.
 					NickAlias *na = NickAlias::Find(subject);
 					if (na && !na->nick.empty() && !na->nc->HasExt("NS_SUSPENDED") && !na->nc->HasExt("UNCONFIRMED"))
 					{
@@ -178,7 +173,7 @@ public:
 						return true;
 					}
 				}
-				// JWT verification failed or account not found
+				// JWT verification failed or account not found.
 				Log(this->owner, "sasl", Config->GetClient("NickServ"))
 					<< sess->GetUserInfo() << " failed to identify using JWT token";
 				SASL::service->Fail(sess);
@@ -186,8 +181,24 @@ public:
 			}
 			else
 			{
-				// Standard password authentication
-				SASL::IdentifyRequest *req = new SASL::IdentifyRequest(this->owner, m.source, authcid, passwd, sess->hostname, sess->ip);
+				// Standard password authentication.
+				class SASLIdentifyRequest : public IdentifyRequest {
+				public:
+					SASL::Session *sess;
+					SASLIdentifyRequest(Module *m, const Anope::string &acc, const Anope::string &pass, const Anope::string &ip, SASL::Session *s)
+						: IdentifyRequest(m, acc, pass, ip), sess(s) {}
+					void OnSuccess() override {
+						NickCore *nc = nullptr;
+						NickAlias *na = NickAlias::Find(this->GetAccount());
+						if (na)
+							nc = na->nc;
+						SASL::service->Succeed(sess, nc);
+					}
+					void OnFail() override {
+						SASL::service->Fail(sess);
+					}
+				};
+				SASLIdentifyRequest *req = new SASLIdentifyRequest(this->owner, authcid, passwd, sess->ip, sess);
 				FOREACH_MOD(OnCheckAuthentication, (NULL, req));
 				req->Dispatch();
 			}
@@ -196,27 +207,24 @@ public:
 	}
 };
 
-class External final
-	: public SASL::Mechanism
+// External mechanism for certificate-based authentication.
+class External final : public SASL::Mechanism
 {
 private:
 	ServiceReference<CertService> certs;
 
-	struct Session final
-		: SASL::Session
+	struct Session final : public SASL::Session
 	{
 		std::vector<Anope::string> certs;
-
 		Session(SASL::Mechanism *m, const Anope::string &u) : SASL::Session(m, u) { }
 	};
 
 public:
 	External(Module *o)
-		: SASL::Mechanism(o, "EXTERNAL")
-		, certs("CertService", "certs")
+		: SASL::Mechanism(o, "EXTERNAL"), certs("CertService", "certs")
 	{
 		if (!IRCD || !IRCD->CanCertFP)
-			throw ModuleException("No CertFP");
+			throw ModuleException("No CertFP support");
 	}
 
 	Session *CreateSession(const Anope::string &uid) override
@@ -227,38 +235,33 @@ public:
 	bool ProcessMessage(SASL::Session *sess, const SASL::Message &m) override
 	{
 		Session *mysess = anope_dynamic_static_cast<Session *>(sess);
-
 		if (m.type == "S")
 		{
 			mysess->certs.assign(m.data.begin() + 1, m.data.end());
-
 			SASL::service->SendMessage(sess, "C", "+");
 		}
 		else if (m.type == "C")
 		{
 			if (!certs || mysess->certs.empty())
 				return false;
-
 			for (auto it = mysess->certs.begin(); it != mysess->certs.end(); ++it)
 			{
 				auto *nc = certs->FindAccountFromCert(*it);
 				if (nc && !nc->HasExt("NS_SUSPENDED") && !nc->HasExt("UNCONFIRMED"))
 				{
-					// If we are using a fallback cert then upgrade it.
+					// If using a fallback cert then upgrade it.
 					if (it != mysess->certs.begin())
 					{
 						auto *cl = nc->GetExt<NSCertList>("certificates");
 						if (cl)
 							cl->ReplaceCert(*it, mysess->certs[0]);
 					}
-
 					Log(this->owner, "sasl", Config->GetClient("NickServ")) << sess->GetUserInfo() << " identified to account " << nc->display << " using SASL EXTERNAL";
 					SASL::service->Succeed(sess, nc);
 					delete sess;
 					return true;
 				}
 			}
-
 			Log(this->owner, "sasl", Config->GetClient("NickServ")) << sess->GetUserInfo() << " failed to identify using certificate " << mysess->certs.front() << " using SASL EXTERNAL";
 			return false;
 		}
@@ -266,8 +269,8 @@ public:
 	}
 };
 
-class Anonymous final
-	: public SASL::Mechanism
+// Anonymous mechanism.
+class Anonymous final : public SASL::Mechanism
 {
 public:
 	Anonymous(Module *o)
@@ -285,11 +288,9 @@ public:
 		{
 			Anope::string decoded;
 			Anope::B64Decode(m.data[0], decoded);
-
 			auto user = sess->GetUserInfo();
 			if (!decoded.empty())
 				user += " [" + decoded + "]";
-
 			Log(this->owner, "sasl", Config->GetClient("NickServ")) << user << " unidentified using SASL ANONYMOUS";
 			SASL::service->Succeed(sess, nullptr);
 		}
@@ -297,9 +298,8 @@ public:
 	}
 };
 
-class SASLService final
-	: public SASL::Service
-	, public Timer
+// Core SASL service that handles sessions and communication.
+class SASLService final : public SASL::Service, public Timer
 {
 private:
 	Anope::map<std::pair<time_t, unsigned short>> badpasswords;
@@ -307,8 +307,7 @@ private:
 
 public:
 	SASLService(Module *o)
-		: SASL::Service(o)
-		, Timer(o, 60, true)
+		: SASL::Service(o), Timer(o, 60, true)
 	{
 	}
 
@@ -321,8 +320,7 @@ public:
 	void ProcessMessage(const SASL::Message &m) override
 	{
 		if (m.data.empty())
-			return; // Malformed.
-
+			return;
 		if (m.target != "*")
 		{
 			Server *s = Server::Find(m.target);
@@ -342,7 +340,6 @@ public:
 			if (!mech)
 			{
 				SASL::Session tmp(NULL, m.source);
-
 				this->SendMechs(&tmp);
 				this->Fail(&tmp);
 				return;
@@ -351,7 +348,6 @@ public:
 			Anope::string hostname, ip;
 			if (session)
 			{
-				// Copy over host/ip to mech-specific session
 				hostname = session->hostname;
 				ip = session->ip;
 				delete session;
@@ -362,7 +358,6 @@ public:
 			{
 				session->hostname = hostname;
 				session->ip = ip;
-
 				sessions[m.source] = session;
 			}
 		}
@@ -404,9 +399,7 @@ public:
 	SASL::Session *GetSession(const Anope::string &uid) override
 	{
 		auto it = sessions.find(uid);
-		if (it != sessions.end())
-			return it->second;
-		return NULL;
+		return it != sessions.end() ? it->second : NULL;
 	}
 
 	void RemoveSession(SASL::Session *sess) override
@@ -416,7 +409,7 @@ public:
 
 	void DeleteSessions(SASL::Mechanism *mech, bool da) override
 	{
-		for (auto it = sessions.begin(); it != sessions.end();)
+		for (auto it = sessions.begin(); it != sessions.end(); )
 		{
 			auto del = it++;
 			if (*del->second->mech == mech)
@@ -435,14 +428,11 @@ public:
 		msg.target = session->uid;
 		msg.type = mtype;
 		msg.data.push_back(data);
-
 		SASL::protocol_interface->SendSASLMessage(msg);
 	}
 
 	void Succeed(SASL::Session *session, NickCore *nc) override
 	{
-		// If the user is already introduced then we log them in now.
-		// Otherwise, we send an SVSLOGIN to log them in later.
 		User *user = User::Find(session->uid);
 		NickAlias *na = nc ? nc->na : nullptr;
 		if (user)
@@ -462,27 +452,22 @@ public:
 	void Fail(SASL::Session *session) override
 	{
 		this->SendMessage(session, "D", "F");
-
-		auto *u = User::Find(session->uid);
+		User *u = User::Find(session->uid);
 		if (u)
 		{
 			u->BadPassword();
 			return;
 		}
-
 		const auto badpasslimit = Config->GetBlock("options").Get<int>("badpasslimit");
 		if (!badpasslimit)
 			return;
-
 		auto it = badpasswords.find(session->uid);
 		if (it == badpasswords.end())
 			it = badpasswords.emplace(session->uid, std::make_pair(0, 0)).first;
 		auto &[invalid_pw_time, invalid_pw_count] = it->second;
-
 		const auto badpasstimeout = Config->GetBlock("options").Get<time_t>("badpasstimeout");
 		if (badpasstimeout > 0 && invalid_pw_time > 0 && invalid_pw_time < Anope::CurTime - badpasstimeout)
 			invalid_pw_count = 0;
-
 		invalid_pw_count++;
 		invalid_pw_time = Anope::CurTime;
 		if (invalid_pw_count >= badpasslimit)
@@ -498,7 +483,6 @@ public:
 		Anope::string buf;
 		for (const auto &mech : mechs)
 			buf += "," + mech;
-
 		this->SendMessage(session, "M", buf.empty() ? "" : buf.substr(1));
 	}
 
@@ -512,7 +496,6 @@ public:
 			else
 				it++;
 		}
-
 		for (auto it = sessions.begin(); it != sessions.end(); )
 		{
 			const auto [uid, sess] = *it++;
@@ -525,14 +508,13 @@ public:
 	}
 };
 
-class ModuleSASLJWT final
-	: public Module
+class ModuleSASLJWT final : public Module
 {
 	SASLService sasl;
 
 	Anonymous anonymous;
-	JWTEnhancedPlain plain;  // Using our enhanced PLAIN mechanism
-	JWT jwt;                 // Adding our JWT mechanism
+	JWTEnhancedPlain plain;  // Using enhanced PLAIN that handles JWT tokens.
+	JWT jwt;                 // JWT mechanism.
 	External *external = nullptr;
 
 	std::vector<Anope::string> mechs;
@@ -542,10 +524,7 @@ class ModuleSASLJWT final
 		std::vector<Anope::string> newmechs = ::Service::GetServiceKeys("SASL::Mechanism");
 		if (newmechs == mechs || !SASL::protocol_interface)
 			return;
-
 		mechs = newmechs;
-
-		// If we are connected to the network then broadcast the mechlist.
 		if (Me && Me->IsSynced())
 			SASL::protocol_interface->SendSASLMechanisms(mechs);
 	}
@@ -588,10 +567,10 @@ public:
 
 	void OnPreUplinkSync(Server *) override
 	{
-		// We have not yet sent a mechanism list so always do it here.
 		if (SASL::protocol_interface)
 			SASL::protocol_interface->SendSASLMechanisms(mechs);
 	}
 };
 
 MODULE_INIT(ModuleSASLJWT)
+
